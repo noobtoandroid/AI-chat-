b44ad01686b6235e965b4e4a36031bfe796e81a2
noobtoandroid <32996842+noobtoandroid@users.noreply.github.com>
2026-05-22 23:22:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,14 +26,7 @@ class LiteWorkspaceApp extends StatelessWidget {
           onSurface: const Color(0xFF1D1B1E),
         ),
       ),
-      builder: (context, child) {
-        return MediaQuery(
-          data: MediaQuery.of(context).copyWith(
-            textScaler: TextScaler.noScaling,
-          ),
-          child: child!,
-        );
-      },
+  
       home: const ScaffoldingPage(),
     );
   }
